22e5f5598c102654cfcf1d139183196f11cfe762
Ayush Arora <aarora7@dons.usfca.edu>
2020-08-15 23:22:52 -0700

    skeleton updated

--- src/main/java/primitives/ArrayList_description.java
+++
@@ -1,7 +0,0 @@
-package primitives;
-
-public class ArrayList_description {
-    public static void main(String[] args) {
-        System.out.println("ayush");
-    }
-}
---
+++ src/main/java/wrapper/ArrayList_.java
@@ -0,0 +1,14 @@
+package wrapper;
+
+import java.util.ArrayList;
+
+public class ArrayList_ {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        for(Integer a: arrayList){
+            System.out.println("a "+a);
+        }
+        System.out.println("ayush");
+    }
+}
